09ce4bcbd87db0bf6fc13d32c1af5de5587903be
kostasfitz <kostasfitzios@gmail.com>
2022-11-16 11:30:23 +0200

    main can call multiple scrapers tennis scraper ready, date to be fixed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tennis_scrape import tennis_scrape
 
 # scraper setup
 driver_path = 'C:\Drivers\chromedriver_win32\chromedriver.exe'
-website = 'https://www.interwetten.gr/en/sport/leaguelist?leagueIds=408826,407726'
+# insert link of daily tennis coupon
+website = 'https://www.interwetten.gr/en/sportsbook/l/115125/atp-world-tour-finals'
 
 tennis_scrape(driver_path, website)
